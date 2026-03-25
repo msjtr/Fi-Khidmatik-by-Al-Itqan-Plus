9adeac477dea67bd5d23e3c3b0a33200ec0d3ae4
msjtr <msjt301@gmail.com>
2026-03-25 15:10:54 +0300

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -1,1 +1,39 @@
+let cart = [];
 
+function addToCart(){
+
+let product = {
+name: document.getElementById("product_name").value,
+price: document.getElementById("product_price").value,
+id: document.getElementById("product_id").value,
+code: document.getElementById("product_code").value
+};
+
+cart.push(product);
+renderCart();
+
+}
+
+function renderCart(){
+
+let html = "";
+
+cart.forEach((p,i)=>{
+
+html += `
+<div>
+${p.name} - ${p.price}
+<button onclick="removeItem(${i})">حذف</button>
+</div>
+`;
+
+});
+
+document.getElementById("cart").innerHTML = html;
+
+}
+
+function removeItem(i){
+cart.splice(i,1);
+renderCart();
+}
