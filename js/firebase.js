421d550a2fbbd95181e06ff5a7534279ec37db36
msjtr <msjt301@gmail.com>
2026-03-26 20:21:09 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,1 +1,15 @@
+// Firebase Config
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
 
+// تشغيل Firebase
+firebase.initializeApp(firebaseConfig);
+
+// قاعدة البيانات
+const db = firebase.firestore();
