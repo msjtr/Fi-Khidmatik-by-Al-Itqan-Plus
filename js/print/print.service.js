1fcf73b715d4fa86b499ed94b1e824e9419d7d4a
msjtr <msjt301@gmail.com>
2026-03-30 08:59:57 +0300

    Update print.service.js

--- js/print/print.service.js
+++ js/print/print.service.js
@@ -1,23 +1,22 @@
-export async function printHTML(element) {
+export async function printInvoice(element) {
 
-const win = window.open('', '_blank');
+    const win = window.open('', '_blank');
 
-win.document.write(`
-<html>
-<head>
-<link rel="stylesheet" href="/css/print.css">
-</head>
-<body>
-${element.outerHTML}
-</body>
-</html>
-`);
+    win.document.write(`
+    <html>
+    <head>
+    <link rel="stylesheet" href="/css/print.css">
+    </head>
+    <body>
+    ${element.outerHTML}
+    </body>
+    </html>
+    `);
 
-win.document.close();
+    win.document.close();
 
-win.onload = () => {
-    win.print();
-    win.close();
-};
-
+    win.onload = () => {
+        win.print();
+        win.close();
+    };
 }
