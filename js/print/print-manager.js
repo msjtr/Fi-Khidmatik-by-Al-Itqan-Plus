9ef42f0aa6c3262d32cb3fdf1624b4818fafedf6
msjtr <msjt301@gmail.com>
2026-03-31 22:42:13 +0300

    Update print-manager.js

--- js/print/print-manager.js
+++ js/print/print-manager.js
@@ -1,40 +1,28 @@
-// print-manager.js
 import { buildInvoiceHTML } from './template.js';
 import { printInvoice } from './print.service.js';
 import { generatePDF } from './pdf.service.js';
 import { generateImage } from './image.service.js';
 
-function createInvoiceElement(order, cartRows, totals) {
-  const wrapper = document.createElement('div');
-  wrapper.innerHTML = buildInvoiceHTML(order, cartRows, totals);
-  wrapper.style.position = 'fixed';
-  wrapper.style.top = '-9999px';
-  wrapper.style.left = '-9999px';
-  wrapper.style.opacity = '0';
-  wrapper.style.pointerEvents = 'none';
-  document.body.appendChild(wrapper);
-  return wrapper.firstElementChild;
+function createTempElement(order, cartRows, totals) {
+  const div = document.createElement('div');
+  div.innerHTML = buildInvoiceHTML(order, cartRows, totals);
+  div.style.position = 'fixed';
+  div.style.top = '-9999px';
+  div.style.left = '-9999px';
+  document.body.appendChild(div);
+  return div.firstElementChild;
 }
 
-function prepareInvoiceData(order) {
+function prepareData(order) {
   const cartRows = order.items.map(item => `
-    <tr>
-      <td>${escapeHtml(item.name || '-')}</td>
-      <td>${escapeHtml(item.barcode || '-')}</td>
-      <td>${escapeHtml(item.description || '-')}</td>
-      <td>${item.quantity}</td>
-      <td>${Number(item.price).toFixed(2)}</td>
-      <td>0</td>
-      <td>${(item.price * item.quantity).toFixed(2)}</td>
-    </tr>
+    <tr><td>${escapeHtml(item.name)}</td><td>${escapeHtml(item.barcode || '-')}</td><td>${escapeHtml(item.description || '-')}</td>
+    <td>${item.quantity}</td><td>${item.price.toFixed(2)}</td><td>0</td><td>${(item.price * item.quantity).toFixed(2)}</td></tr>
   `).join('');
-
-  const subtotal = order.items.reduce((s, i) => s + (i.price * i.quantity), 0);
+  const subtotal = order.items.reduce((s, i) => s + i.price * i.quantity, 0);
   const discount = order.discount || 0;
   const afterDiscount = subtotal - discount;
   const tax = afterDiscount * 0.15;
   const total = afterDiscount + tax;
-
   const totals = {
     subtotal: subtotal.toFixed(2) + ' ريال',
     discount: discount.toFixed(2) + ' ريال',
@@ -46,31 +34,26 @@ function prepareInvoiceData(order) {
 
 function escapeHtml(str) {
   if (!str) return '';
-  return str.replace(/[&<>]/g, function(m) {
-    if (m === '&') return '&amp;';
-    if (m === '<') return '&lt;';
-    if (m === '>') return '&gt;';
-    return m;
-  });
+  return str.replace(/[&<>]/g, m => ({ '&':'&amp;', '<':'&lt;', '>':'&gt;' }[m]));
 }
 
 export async function printInvoiceHandler(order) {
-  const { cartRows, totals } = prepareInvoiceData(order);
-  const el = createInvoiceElement(order, cartRows, totals);
+  const { cartRows, totals } = prepareData(order);
+  const el = createTempElement(order, cartRows, totals);
   await printInvoice(el);
   el.parentElement.remove();
 }
 
 export async function exportAsPDF(order) {
-  const { cartRows, totals } = prepareInvoiceData(order);
-  const el = createInvoiceElement(order, cartRows, totals);
+  const { cartRows, totals } = prepareData(order);
+  const el = createTempElement(order, cartRows, totals);
   await generatePDF(el, order);
   el.parentElement.remove();
 }
 
 export async function exportAsImage(order) {
-  const { cartRows, totals } = prepareInvoiceData(order);
-  const el = createInvoiceElement(order, cartRows, totals);
+  const { cartRows, totals } = prepareData(order);
+  const el = createTempElement(order, cartRows, totals);
   await generateImage(el, order);
   el.parentElement.remove();
 }
