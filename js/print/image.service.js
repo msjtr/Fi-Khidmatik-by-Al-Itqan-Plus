739b1ad677adeb8937e46e467819ac1fdbbdd7f4
msjtr <msjt301@gmail.com>
2026-03-31 15:58:01 +0300

    Update image.service.js

--- js/print/image.service.js
+++ js/print/image.service.js
@@ -1,12 +1,14 @@
 // image.service.js
 export async function generateImage(element, order) {
-    const canvas = await html2canvas(element, {
-        scale: 3,
-        backgroundColor: '#ffffff',
-        logging: false
-    });
-    const link = document.createElement('a');
-    link.download = `فاتورة_${order.orderNumber || order.id}.png`;
-    link.href = canvas.toDataURL('image/png');
-    link.click();
+  const canvas = await html2canvas(element, {
+    scale: 3,
+    backgroundColor: '#ffffff',
+    logging: false,
+    useCORS: true
+  });
+  
+  const link = document.createElement('a');
+  link.download = `فاتورة_${order.orderNumber || order.id}.png`;
+  link.href = canvas.toDataURL('image/png');
+  link.click();
 }
