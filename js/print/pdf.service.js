735c27ff15716689c241506e3c8e7c0d76f34a62
msjtr <msjt301@gmail.com>
2026-03-31 15:57:15 +0300

    Update pdf.service.js

--- js/print/pdf.service.js
+++ js/print/pdf.service.js
@@ -1,11 +1,12 @@
 // pdf.service.js
 export async function generatePDF(element, order) {
-    const opt = {
-        margin: [0.5, 0.5, 0.5, 0.5],
-        filename: `فاتورة_${order.orderNumber || order.id}.pdf`,
-        image: { type: 'jpeg', quality: 0.98 },
-        html2canvas: { scale: 2, letterRendering: true },
-        jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
-    };
-    await html2pdf().set(opt).from(element).save();
+  const opt = {
+    margin: [0.5, 0.5, 0.5, 0.5],
+    filename: `فاتورة_${order.orderNumber || order.id}.pdf`,
+    image: { type: 'jpeg', quality: 0.98 },
+    html2canvas: { scale: 2, letterRendering: true, useCORS: true },
+    jsPDF: { unit: 'in', format: 'a4', orientation: 'portrait' }
+  };
+  
+  await html2pdf().set(opt).from(element).save();
 }
