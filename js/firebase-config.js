645a8b57e605dabc765bb7ceffb56073a8b92e19
msjtr <msjt301@gmail.com>
2026-04-13 14:24:08 +0300

    Update and rename orders-firebase-db.js to firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,9 @@
+// js/firebase-config.js
+export const firebaseConfig = {
+    apiKey: "AIzaSyBWYW6Qqlhh904pBeuJ29wY7Cyjm2uklBA",
+    authDomain: "msjt301-974bb.firebaseapp.com",
+    projectId: "msjt301-974bb",
+    storageBucket: "msjt301-974bb.firebasestorage.app",
+    messagingSenderId: "186209858482",
+    appId: "1:186209858482:web:186ca610780799ef562aab"
+};
