60f79ffa1fdf52df88eab202ad869ee72b23bf5b
msjtr <msjt301@gmail.com>
2026-04-17 20:09:16 +0300

    Update firebase.js

--- js/core/firebase.js
+++ js/core/firebase.js
@@ -1,6 +1,22 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-import { firebaseConfig } from './config.js';
+/**
+ * إعدادات اتصال منصة تيرا جيتواي - Tera Gateway
+ * مشروع: MSJT301
+ */
 
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const firebaseConfig = {
+    apiKey: "AIzaSyBWYW6Qqlhh904pBeuJ29wY7Cyjm2uklBA",
+    authDomain: "msjt301-974bb.firebaseapp.com",
+    projectId: "msjt301-974bb",
+    storageBucket: "msjt301-974bb.firebasestorage.app",
+    messagingSenderId: "186209858482",
+    appId: "1:186209858482:web:186ca610780799ef562aab",
+    measurementId: "G-NDVGC9GPQZ"
+};
+
+// ملاحظة لمحمد: تأكد من استيراد Firebase بالشكل التالي في ملفات الموديولات
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+// تهيئة الخدمة (إضافة اختيارية لضمان عملها فور الاستدعاء)
+// const app = initializeApp(firebaseConfig);
+// export const db = getFirestore(app);
