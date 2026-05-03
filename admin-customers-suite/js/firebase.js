e5d310813be70ca43b897cefb71735d79ad07be8
msjtr <msjt301@gmail.com>
2026-05-03 11:33:56 +0300

    Add Firebase Authentication to firebase.js

--- admin-customers-suite/js/firebase.js
+++ admin-customers-suite/js/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js"; // أضف هذا السطر
 
 const firebaseConfig = {
     apiKey: "AIzaSyCikh505fV7E7mLjrxQLjvhMnFTJET5mNA",
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const storage = getStorage(app);
+const auth = getAuth(app); // أضف هذا السطر
 
-export { db, storage };
+export { db, storage, auth }; // تأكد من إضافة auth هنا
