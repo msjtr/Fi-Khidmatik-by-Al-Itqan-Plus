8856adf59a8f0a61ac1f577d3c02b70cba7ab785
msjtr <msjt301@gmail.com>
2026-05-03 12:54:05 +0300

    Update firebase.js

--- admin-customers-suite/js/firebase.js
+++ admin-customers-suite/js/firebase.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js"; // أضف هذا السطر
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-auth.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyCikh505fV7E7mLjrxQLjvhMnFTJET5mNA",
@@ -13,10 +13,10 @@ const firebaseConfig = {
     measurementId: "G-91D78EXWXK"
 };
 
-// تهيئة المشروع
+// تهيئة تطبيق فيرميز (Firebase App)
 const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-const auth = getAuth(app); // أضف هذا السطر
 
-export { db, storage, auth }; // تأكد من إضافة auth هنا
+// تصدير الخدمات لاستخدامها في النظام
+export const db = getFirestore(app);
+export const storage = getStorage(app);
+export const auth = getAuth(app);
